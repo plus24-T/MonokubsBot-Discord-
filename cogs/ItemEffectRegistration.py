0322132689f60ed186baba983e45d6debdbe5248
plus24-T <24vsop+github@gmail.com>
2024-09-06 23:17:42 +0900

    アイテム効果登録の追加

--- cogs/ItemEffectRegistration.py
+++ cogs/ItemEffectRegistration.py
@@ -12,29 +12,25 @@ import gv
 
 load_dotenv()
 
-class Add_dead_role(commands.Cog):#Cog名、任意だが分かりやすさのためにコマンドが一つなら頭大文字でクラス作成
+class ItemEffectRegistration(commands.Cog):#Cog名、任意だが分かりやすさのためにコマンドが一つなら頭大文字でクラス作成
     def __init__(self, bot:commands.Bot):
         self.bot = bot
 
     @app_commands.command(
-            name="死亡ロール付与",#coomand_nameがコマンドになる
-            description="襲撃とおしおきの死亡者登録するやつ"#コマンドリストに表示される説明文
+            name="アイテム効果登録",#coomand_nameがコマンドになる
+            description="アイテム効果登録するやつ"#コマンドリストに表示される説明文
             )
-    async def add_dead_role(self, itx:discord.Interaction,死因:Literal["襲撃","おしおき"],死者:discord.Member):#ここが処理内容、必要な引数とか設定する
-        if 死因=="襲撃":
-            gv.kill+=1
-            await itx.response.send_message(f"襲撃により{死者.nick}は死亡しました")
-            死者.remove_roles(discord.utils.get(itx.guild.roles,name="生存"))
-            死者.add_roles(discord.utils.get(itx.guild.roles,name="死亡"))
+    async def item_effect_registration(self, itx:discord.Interaction,効果:Literal["襲撃無効","二票持ち"],対象:discord.Member):#ここが処理内容、必要な引数とか設定する
+        if 効果=="襲撃無効":
+            gv.nick_to_data[対象].escorted=True
+            await itx.response.send_message(f"{対象.nick}に襲撃無効が付与されました")
+
         else:
-            await itx.response.send_message(f"{死者.nick}はおしおきされました")
-            死者.remove_roles(discord.utils.get(itx.guild.roles,name="生存"))
-            死者.add_roles(discord.utils.get(itx.guild.roles,name="死亡"))
-            if gv.nick_to_data[死者.nick].role.name=="超高校級の絶望":
-                await itx.followup.send(f"{死者.nick}は超高校級の絶望でした\n\n{死者.nick}の勝利です")
+            gv.nick_to_data[対象].votes=2
+            await itx.response.send_message(f"{対象.nick}は次の投票で2票投票できます")
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(
-        Add_dead_role(bot),
+        ItemEffectRegistration(bot),
         guilds = [discord.Object(id=os.getenv("GUILD_ID"))]
         )
