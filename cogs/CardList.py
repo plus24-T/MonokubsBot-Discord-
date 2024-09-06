4b21b4accede825ef2e6a0e23847e7886ef43af3
plus24-T <24vsop+github@gmail.com>
2024-09-06 23:51:54 +0900

    カードリストの試作
    paginationと画像付きEmbedの両立方が分からん

--- cogs/CardList.py
+++ cogs/CardList.py
@@ -5,41 +5,26 @@ from dotenv import load_dotenv
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord.ext.ui import View,ViewTracker, MessageProvider,PageView,PaginationView,Message
 
-load_dotenv()
-#試作中　動かないよ！
+import Paginator
 
-
-class Page(PageView):
-    def __init__(self, content: str):
-        super(Page, self).__init__()
-        self.content = content
-
-    async def body(self) -> Message | View:
-        return  Message(self.content)
-
-    async def on_appear(self, paginator: PaginationView) -> None:
-        print(f"appeared page: {paginator.page}")
-
+#試作中！
+# Create a list of embeds to paginate.
+embeds = [discord.Embed(title="First embed"),
+          discord.Embed(title="Second embed"),
+          discord.Embed(title="Third embed")
+          ]
 class CardList(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
+
 
     @app_commands.command(
             name="card_list",#comand_nameがコマンドになる
             description="ページ送り型のカードリスト出す奴"#コマンドリストに表示される説明文
             )
     async def card_list(self, itx:discord.Interaction):#ここが処理内容、必要な引数とか設定する
-        view = Page([
-            Page("The first page -- Morning --"),
-            Page("The second page -- Noon --"),
-            Page("The third page -- Afternoon --"),
-            Page("The forth page -- Evening --"),
-            Page("The last page -- Good night! --"),
-        ])
-        tracker = ViewTracker(view, timeout=None)
-        await tracker.track(MessageProvider(itx.channel))
+        await Paginator.Simple().start(itx, pages=embeds)
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(
