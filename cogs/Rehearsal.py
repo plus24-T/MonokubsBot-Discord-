9ec7f7b302532af4f4ac878acd5afa8afe804d85
plus24-T <24vsop+github@gmail.com>
2024-09-23 22:02:04 +0900

    interactionの省略itxを未省略形に変更

--- cogs/Rehearsal.py
+++ cogs/Rehearsal.py
@@ -18,21 +18,21 @@ class Rehearsal_Select(discord.ui.Select):
             options=options,
             disabled=False
             )
-    async def callback(self, itx: discord.Interaction):
-        await itx.response.send_message(
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
             f"『{self.values[0]}』の部屋を荒らしました\n"
-            f"{discord.utils.get(itx.guild.channels,name="食堂").mention}へお戻りください"
+            f"{discord.utils.get(interaction.guild.channels,name="食堂").mention}へお戻りください"
             )
         gv.table_data.day_count+=1
-        await discord.utils.get(itx.guild.channels,name=self.values[0]).send(
+        await discord.utils.get(interaction.guild.channels,name=self.values[0]).send(
             "あなたの部屋が荒らされました、手持ちのアイテムを1枚選択して裏向きのまま捨てて下さい\n"
             "捨て終わったら【捨てました】ボタンを押してください",
             view=views.RehearsalEndConfirmationButton(self.bot)
             )
-        await discord.utils.get(itx.guild.channels,name="食堂").send(
+        await discord.utils.get(interaction.guild.channels,name="食堂").send(
             f"オハヨウゴザイマス\n{gv.table_data.day_count}日目の朝時間になりました"
             )
-        await discord.utils.get(itx.guild.channels,name="食堂").send(
+        await discord.utils.get(interaction.guild.channels,name="食堂").send(
             f"{self.values[0]}の部屋が荒らされました\n"
             "処理が終わるまでしばらくお待ちください")
         self.disabled=True
@@ -51,16 +51,16 @@ class Rehearsal(commands.Cog):#コマンド名、頭大文字でクラス作成
             name="rehearsal",#coomand_nameがコマンドになる
             description="0日目夜のクロの下見を行います"#コマンドリストに表示される説明文
             )
-    async def rehearsal(self, itx:discord.Interaction):#ここが処理内容、必要な引数とか設定する
-        living_members = discord.utils.get(itx.guild.roles,name="生存").members
+    async def rehearsal(self, interaction:discord.Interaction):#ここが処理内容、必要な引数とか設定する
+        living_members = discord.utils.get(interaction.guild.roles,name="生存").members
         select_op_living_members = []    #生存メンバーのリストから選択候補のリストを作成
         for member in living_members:
             select_op_living_members.append(discord.SelectOption(label=member.nick))
-        await itx.response.send_message(
+        await interaction.response.send_message(
             "クロが下見の対象を選択しています\n"
             "（クロは自身の個室へ移動して対象を選択してください）"
             )
-        await discord.utils.get(itx.guild.channels,name=gv.chara_role_list.kuro[0].nick).send(
+        await discord.utils.get(interaction.guild.channels,name=gv.chara_role_list.kuro[0].nick).send(
             "下見の対象を選択してください",
             view=Rehearsal_View(options=select_op_living_members,bot=self.bot)
             )
