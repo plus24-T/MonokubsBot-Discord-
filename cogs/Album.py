3f2f05eaaeb5e3328535a2fd485cd5525aae4709
plus24-T <24vsop+github@gmail.com>
2024-09-23 23:25:21 +0900

    略称開き

--- cogs/Album.py
+++ cogs/Album.py
@@ -14,13 +14,13 @@ class Album_Select(discord.ui.Select):
             options=options,
             disabled=False
             )
-    async def callback(self, itx: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
         #プレイヤー数による患者との分岐は後回し（たぶんしばらく要らないため）
         if gv.get_chara_data(self.values[0]).role == gv.CharaRole.URAGIRI:
-            await itx.response.send_message(f"『{self.values[0]}』は 裏切者 です")
+            await interaction.response.send_message(f"『{self.values[0]}』は 裏切者 です")
         else:
-            await itx.response.send_message(f"『{self.values[0]}』は 裏切者 ではありません")
-        await discord.utils.get(itx.guild.channels,name="食堂").send("調査が終わりました")
+            await interaction.response.send_message(f"『{self.values[0]}』は 裏切者 ではありません")
+        await discord.utils.get(interaction.guild.channels,name="食堂").send("調査が終わりました")
         self.disabled=True
 
 class Album_View(discord.ui.View):
@@ -37,13 +37,13 @@ class Album(commands.Cog):#Cog名、頭大文字でクラス作成
             name="album",#coomand_nameがコマンドになる
             description="誰かの卒業アルバムを使用します"#コマンドリストに表示される説明文            
             )
-    async def album(self,itx:discord.Interaction): 
-        living_members = discord.utils.get(itx.guild.roles,name="生存").members
+    async def album(self,interaction:discord.Interaction): 
+        living_members = discord.utils.get(interaction.guild.roles,name="生存").members
         select_op_living_members = []    #生存メンバーのリストから選択候補のリストを作成
         for member in living_members:
             select_op_living_members.append(discord.SelectOption(label=member.nick))
-        await itx.response.send_message(f"{itx.user.nick}が誰かの卒業アルバムを調べています、少々お待ちください")
-        await discord.utils.get(itx.guild.channels,name=itx.user.nick).send(
+        await interaction.response.send_message(f"{interaction.user.nick}が誰かの卒業アルバムを調べています、少々お待ちください")
+        await discord.utils.get(interaction.guild.channels,name=interaction.user.nick).send(
             "判別の対象を選択してください",
             view=Album_View(options=select_op_living_members)
             )
