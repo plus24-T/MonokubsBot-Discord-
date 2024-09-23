d9a669e801efe2d853d0a7ac8b8f8a8612b417fe
plus24-T <24vsop+github@gmail.com>
2024-09-23 23:19:33 +0900

    略称開き

--- cogs/Hagakure_ability.py
+++ cogs/Hagakure_ability.py
@@ -14,13 +14,13 @@ class Hagakure_Select(discord.ui.Select):
             options=options,
             disabled=False
             )
-    async def callback(self, itx: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
         #プレイヤー数による患者との分岐は後回し（たぶんしばらく要らないため）
         if gv.get_chara_data(self.values[0]).role == gv.CharaRole.MONOMI:
-            await itx.response.send_message(f"『{self.values[0]}』は モノミ だべ！")
+            await interaction.response.send_message(f"『{self.values[0]}』は モノミ だべ！")
         else:
-            await itx.response.send_message(f"『{self.values[0]}』は モノミ じゃねえべ！")
-        await discord.utils.get(itx.guild.channels,name="食堂").send("占いが終わりました")
+            await interaction.response.send_message(f"『{self.values[0]}』は モノミ じゃねえべ！")
+        await discord.utils.get(interaction.guild.channels,name="食堂").send("占いが終わりました")
         self.disabled=True
 
 class Hagakure_View(discord.ui.View):
@@ -37,13 +37,13 @@ class Hagakure(commands.Cog):#コマンド名、頭大文字でクラス作成
             name="hagakure",#coomand_nameがコマンドになる
             description="葉隠の能力を使用します"#コマンドリストに表示される説明文            
             )
-    async def hagakure(self,itx:discord.Interaction): 
-        living_members = discord.utils.get(itx.guild.roles,name="生存").members
+    async def hagakure(self,interaction:discord.Interaction): 
+        living_members = discord.utils.get(interaction.guild.roles,name="生存").members
         select_op_living_members = []    #生存メンバーのリストから選択候補のリストを作成
         for member in living_members:
             select_op_living_members.append(discord.SelectOption(label=member.nick))
-        await itx.response.send_message("葉隠康比呂が占っています、少々お待ちください")
-        await discord.utils.get(itx.guild.channels,name="葉隠康比呂").send(
+        await interaction.response.send_message("葉隠康比呂が占っています、少々お待ちください")
+        await discord.utils.get(interaction.guild.channels,name="葉隠康比呂").send(
             "判別の対象を選択してください",
             view=Hagakure_View(options=select_op_living_members)
             )
