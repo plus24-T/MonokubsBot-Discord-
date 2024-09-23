89e8a87317b14e9cdc8a0c964f750c05608694ca
plus24-T <24vsop+github@gmail.com>
2024-09-23 23:33:18 +0900

    略称開き

--- cogs/Megane.py
+++ cogs/Megane.py
@@ -14,15 +14,15 @@ class Megane_Select(discord.ui.Select):
             options=options,
             disabled=False
             )
-    async def callback(self, itx: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
         target_chara_name = self.values[0]
-        is_despair = utils.check_despair(itx, target_chara_name)
+        is_despair = utils.check_despair(interaction, target_chara_name)
         if is_despair:
-            await itx.response.send_message(f"『{target_chara_name}』は絶望〈ゼツボウ〉サイドです")
+            await interaction.response.send_message(f"『{target_chara_name}』は絶望〈ゼツボウ〉サイドです")
         else:
-            await itx.response.send_message(f"『{target_chara_name}』は希望〈キボウ〉サイドです")
+            await interaction.response.send_message(f"『{target_chara_name}』は希望〈キボウ〉サイドです")
         
-        await discord.utils.get(itx.guild.channels,name="食堂").send("判別が終わりました")
+        await discord.utils.get(interaction.guild.channels,name="食堂").send("判別が終わりました")
         self.disabled=True
 
 class Megane_View(discord.ui.View):
@@ -39,16 +39,16 @@ class Megane(commands.Cog):#コマンド名、頭大文字でクラス作成
             name="megane",#coomand_nameがコマンドになる
             description="おでこのメガネを使用します"#コマンドリストに表示される説明文            
             )
-    async def megane(self,itx:discord.Interaction):
-        if gv.chara_role_list.alterego[0] in discord.utils.get(itx.guild.roles,name="生存").members:
-            await itx.response.send_message("おでこのメガネで、デコデコ、デコリ～ン！\n（使用条件：アルターエゴの死亡　を満たしていません）")
+    async def megane(self,interaction:discord.Interaction):
+        if gv.chara_role_list.alterego[0] in discord.utils.get(interaction.guild.roles,name="生存").members:
+            await interaction.response.send_message("おでこのメガネで、デコデコ、デコリ～ン！\n（使用条件：アルターエゴの死亡　を満たしていません）")
             pass 
-        living_members = discord.utils.get(itx.guild.roles,name="生存").members
+        living_members = discord.utils.get(interaction.guild.roles,name="生存").members
         select_op_living_members = []    #生存メンバーのリストから選択候補のリストを作成
         for member in living_members:
             select_op_living_members.append(discord.SelectOption(label=member.nick))
-        await itx.response.send_message(f"{itx.user.nick}がおでこのメガネの力を行使しています、少々お待ちください")
-        await discord.utils.get(itx.guild.channels,name=itx.user.nick).send(
+        await interaction.response.send_message(f"{interaction.user.nick}がおでこのメガネの力を行使しています、少々お待ちください")
+        await discord.utils.get(interaction.guild.channels,name=interaction.user.nick).send(
             "判別の対象を選択してください",
             view=Megane_View(options=select_op_living_members)
             )
